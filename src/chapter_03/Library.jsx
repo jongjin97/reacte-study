053d5a65b12f2738b33c0cf50fcfdeb358eb6639
jongjin97 <kakao1302@naver.com>
2023-02-28 20:36:05 +0900

    JSX

---
+++ src/chapter_03/Library.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Book from "./Book";
+
+function Library(props) {
+    return (
+        <div>
+            <Book name={"처음 만난 파이썬"} numOfpage={300} />
+            <Book name="처음 만난 파이썬" numOfpage={300} />
+            <Book name="처음 만난 AWS" numOfpage={400} />
+            <Book name="처음 만난 리액트" numOfpage={500} />
+            <Book name="처음 만난 머신러닝" numOfpage={600} />
+        </div>
+    );
+}
+
+export default Library;
