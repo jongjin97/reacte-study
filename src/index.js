d74d54869f29f62062a6fb70a7f2240f285ca1d9
jongjin97 <kakao1302@naver.com>
2023-02-28 23:48:56 +0900

    State and Lifecycle

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import CommentList from "./chapter_05/CommentList";
+import NotificationList from "./chapter_06/NotificationList";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <CommentList/>
+        <NotificationList />
     </React.StrictMode>
 );
 
