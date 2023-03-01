81dbc16ce3f58cb6ef3c01116902fc4419dbaa7a
jongjin97 <kakao1302@naver.com>
2023-03-01 17:01:22 +0900

    Conditional Rendering

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import FunctionConfirmButton from "./chapter_08/FunctionConfirmButton";
+import LandingPage from "./chapter_09/LandingPage";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <FunctionConfirmButton />
+        <LandingPage />
     </React.StrictMode>
 );
 
