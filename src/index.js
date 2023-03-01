a3e66efb7827ab076da0cf644abb7cff13953742
jongjin97 <kakao1302@naver.com>
2023-03-01 18:24:25 +0900

    Forms

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AttendanceBook from "./chapter_10/AttendanceBook";
+import SignUp from './chapter_11/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <AttendanceBook />
+        <SignUp />
     </React.StrictMode>
 );
 
